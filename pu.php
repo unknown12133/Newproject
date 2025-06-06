f54566c60cd63882b8585708be8211883d8d2481
unknown12133 <mr.unknown.boy.022@gmail.com>
2025-06-06 17:56:32 +0530

    Add files via upload

--- pu.php
+++ pu.php
@@ -1,9 +1,9 @@
 <?php
 
  $db='sql12782686';
- $con=new mysqli('sql12.freesqldatabase.com',$db,'unknown@1',$db);
+ //$con=new mysqli('sql12.freesqldatabase.com',$db,'unknown@1',$db);
 
-//$con=new mysqli('localhost','root','','db');
+$con=new mysqli('localhost','root','','db');
 
  header('Content-Type: application/json');
 
@@ -11,37 +11,42 @@
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);}
 else{  
-        $a='';// $aaa='0'; 
-        $b=''; $aab='0';
-        $c=''; $aac='0';
+        $a=''; 
+        $b='';
+        $c='';
+        $quantity=0;
+        $price='0';
    if(isset($_GET['a'])){
     $a=$_GET['a'];
    }
    if(isset($_GET['b'])){
        $b=$_GET['b'];
     }
-    if(isset($_GET['c'])){
-     $c=$_GET['c'];
-    }
 }
-if($a!='' and $b!='' and $c!=''){
-    //$aaa='1';
+if($a!='' and $b!=''){
+    // echo '1';
     $sql="SELECT AMOUNT FROM aaa WHERE UID='$a'";
     $result=mysqli_query($con,$sql);
     $noofrows=mysqli_num_rows($result);
     foreach ($result as $record){
-        $aa=htmlspecialchars($record['AMOUNT']);}
+        $balance=htmlspecialchars($record['AMOUNT']);}
     if($noofrows==1){
-        $ab=$aa-$c;
-        $aab='1';
+    $sql1="SELECT PRICE FROM a";
+    $result1=mysqli_query($con,$sql1);
+    
+    }
+        $ab=$balance-$b;
         if($ab>=0){
-        $sql1="INSERT INTO aa(`UID`, `QUANTITY`, `AMOUNT`,`DATE`,`TIME`)VALUES('$a', '$b', '$c',CURRENT_DATE(),CURRENT_TIME())";
+        foreach ($result1 as $recor){
+        $price=htmlspecialchars($recor['PRICE']);
+
+        $quantity=$b/$price;
+        $sql1="INSERT INTO aa(`UID`, `QUANTITY`, `AMOUNT`,`DATE`,`TIME`)VALUES('$a', '$quantity', '$b',CURRENT_DATE(),CURRENT_TIME())";
         $resul=mysqli_query($con,$sql1);
         $sql2="UPDATE aaa  SET AMOUNT='$ab' WHERE UID='$a'";
         $resu=mysqli_query($con,$sql2);
        
-        $aac='1'; 
       
     }}}
-    echo json_encode(array($aab,$aac));
+    echo json_encode(array($price,$quantity));
     
